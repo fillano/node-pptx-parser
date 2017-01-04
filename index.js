18dcbd79e66ed2c0fcce422b7f7215a27bb7093c
Hsu Ping Feng <fillano.feng@gmail.com>
2017-01-04 21:46:40 +0800

    fix issues with master slide data.

--- index.js
+++ index.js
@@ -18,13 +18,14 @@ function parser(data) {
 	let types = require('./lib/typeParser')(entries, pptx);
 	let presentation = require('./lib/presentationParser')(entries, pptx, relations);
 	let slideMasters = require('./lib/slideMasterParser')(entries, pptx, relations, presentation);
-	console.log(JSON.stringify(slideMasters['ppt/slideMasters/slideMaster1.xml'].commonSlideData, null, 2));
+	//console.log(JSON.stringify(slideMasters['ppt/slideMasters/slideMaster1.xml'].commonSlideData, null, 2));
 	let slideLayouts = require('./lib/slideLayoutParser')(entries, pptx, relations, presentation, slideMasters)
 	let slides = require('./lib/slidesParser')(entries, pptx, relations, presentation, slideMasters, slideLayouts);
 	return {
 		relations: relations,
 		types: types,
 		presentation: presentation,
+		slideMasters: slideMasters,
 		slides: slides
 	};
 }
