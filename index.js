7e3069783a80aaaa342f7229636fc4dd2a34f596
Hsu Ping Feng <fillano.feng@gmail.com>
2017-01-06 15:05:46 +0800

    fix more parsing issues. slide parsing done. but more fix required.

--- index.js
+++ index.js
@@ -17,15 +17,26 @@ function parser(data) {
 	let relations = require('./lib/relationParser')(entries, pptx);
 	let types = require('./lib/typeParser')(entries, pptx);
 	let presentation = require('./lib/presentationParser')(entries, pptx, relations);
-	let slideMasters = require('./lib/slideMasterParser')(entries, pptx, relations, presentation);
-	//console.log(JSON.stringify(slideMasters['ppt/slideMasters/slideMaster1.xml'].commonSlideData, null, 2));
-	let slideLayouts = require('./lib/slideLayoutParser')(entries, pptx, relations, presentation, slideMasters)
-	let slides = require('./lib/slidesParser')(entries, pptx, relations, presentation, slideMasters, slideLayouts);
-	return {
+	let themes = require('./lib/themeParser')(entries, pptx, presentation);
+	//console.log(JSON.stringify(themes, null, 2));
+	let slideMasters = require('./lib/slideMasterParser')(entries, pptx, relations, presentation, themes);
+	//console.log(JSON.stringify(slideMasters, null, 2));
+	let slideLayouts = require('./lib/slideLayoutParser')(entries, pptx, relations, presentation, themes, slideMasters);
+	//console.log(JSON.stringify(slideLayouts['ppt/slideLayouts/slideLayout2.xml'], null, 2));
+	let slides = require('./lib/slidesParser')(entries, pptx, relations, presentation, themes, slideMasters, slideLayouts);
+	//console.log(JSON.stringify(slides['rId4'], null, 2));
+
+	let ret = {
 		relations: relations,
 		types: types,
 		presentation: presentation,
-		slideMasters: slideMasters,
 		slides: slides
 	};
+
+	const fs = require('fs');
+	fs.writeFile('main.json', JSON.stringify(ret, null, 2), 'utf-8', err => {
+		if(!!err) console.error(err);
+	});
+
+	return ret;
 }
